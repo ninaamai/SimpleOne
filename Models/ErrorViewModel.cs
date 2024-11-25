842da40bc021a685a8f8d1d2ca132ef4aedbc254
Nina <ninaamai@mail.ru>
2024-11-25 21:27:57 +0300

    layout
    feat: layout

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleOne_Maiboroda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
